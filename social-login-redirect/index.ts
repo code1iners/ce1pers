90c1aae0dc804678450e5a822c5b02f193c3c01c
__coma__ <codeliner96@gmail.com>
2025-11-02 00:21:11 +0900

    feat(social-login-redirect): 모든 로그인 함수 추가

--- social-login-redirect/index.ts
+++ social-login-redirect/index.ts
@@ -1,1 +1,6 @@
 export { appleLogin } from './src/apple-login.js';
+export { facebookLogin } from './src/facebook-login.js';
+export { googleLogin } from './src/google-login.js';
+export { kakaoLogin } from './src/kakao-login.js';
+export { lineLogin } from './src/line-login.js';
+export { naverLogin } from './src/naver-login.js';
